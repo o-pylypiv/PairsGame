636792e7d6fcdee3631809655d60799977f52261
o-pylypiv <bassenia@gmail.com>
2024-05-30 13:01:58 +0300

    Initial Commit

---
+++ PairsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PairsGame
+//
+//  Created by Olha Pylypiv on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
